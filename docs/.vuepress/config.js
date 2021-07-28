a25b04dfc206101c29e46dcd0554b4b3b5bd1edf
123flight <15855477602@163.com>
2021-07-28 22:09:36 +0800

    配置base

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: CKX
+ * @Date: 2021-07-28 21:50:32
+ */
+
+module.exports = {
+  base: '/notes/',
+  lang: 'en-US',
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+}
