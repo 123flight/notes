aa0d4b1d41335e3736b351f5c2cade11a7d0cbdb
木木夕 <15855477602@163.com>
2021-10-20 19:49:07 +0800

    #FEAT 添加导航栏和侧边栏和一个logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,4 +3,18 @@ module.exports = {
   lang: 'en-US',
   title: 'Hello VuePress',
   description: 'Just playing around',
+
+  themeConfig: {
+    // 导航栏
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/123flight', target:'_self', rel:'' },
+    ],
+    // 侧边栏
+    sidebar: [
+      '/'
+    ],
+    // 最后更新的时间
+    lastUpdated: 'Last Updated',
+    logo: '/public/images/happy.png',
+  }
 }
